d8b76726a8d11d73172f3c2fc35523403f17f9d6
JoseIgnacioTapia <josignaciot@gmail.com>
2025-03-10 16:49:26 -0300

    Product Card Component

--- components/shared/product/ProductList.tsx
+++ components/shared/product/ProductList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ProductCard from './ProductCard';
 
 function ProductList({
   data,
@@ -17,7 +18,7 @@ function ProductList({
       {data.length > 0 ? (
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
           {limitedData.map((product: any) => (
-            <div>{product.name}</div>
+            <ProductCard key={product.slug} product={product} />
           ))}
         </div>
       ) : (
