459d879fd53ce98191c97b677f7bb2bc049ff784
JoseIgnacioTapia <josignaciot@gmail.com>
2025-03-18 21:07:12 -0300

    Zod Validation & Type Inference

--- components/shared/product/ProductList.tsx
+++ components/shared/product/ProductList.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ProductCard from './ProductCard';
+import { Product } from '@/types';
 
 function ProductList({
   data,
   title,
   limit,
 }: {
-  data: any;
+  data: Product[];
   title?: string;
   limit?: number;
 }) {
@@ -17,7 +18,7 @@ function ProductList({
       <h2 className='h2-hold mb-4'>{title}</h2>
       {data.length > 0 ? (
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-          {limitedData.map((product: any) => (
+          {limitedData.map((product: Product) => (
             <ProductCard key={product.slug} product={product} />
           ))}
         </div>
