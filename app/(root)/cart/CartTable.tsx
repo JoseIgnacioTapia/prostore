6f29c1dcfbd8208a862d3db861dc1f0edc0ce0f3
JoseIgnacioTapia <josignaciot@gmail.com>
2025-07-14 20:19:20 -0300

    Add Subtotal Card

--- app/(root)/cart/CartTable.tsx
+++ app/(root)/cart/CartTable.tsx
@@ -13,9 +13,11 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
 import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.action';
 import { Cart } from '@/types';
-import { Button } from '@/components/ui/button';
+import { formatCurrency } from '@/lib/utils';
 
 function CartTable({ cart }: { cart?: Cart }) {
   const router = useRouter();
@@ -36,7 +38,7 @@ function CartTable({ cart }: { cart?: Cart }) {
                 <TableRow>
                   <TableHead>Item</TableHead>
                   <TableHead className='text-center'>Quantity</TableHead>
-                  <TableHead className='text-price'>Price</TableHead>
+                  <TableHead className='text-center'>Price</TableHead>
                 </TableRow>
               </TableHeader>
               <TableBody>
@@ -125,6 +127,31 @@ function CartTable({ cart }: { cart?: Cart }) {
               </TableBody>
             </Table>
           </div>
+
+          <Card>
+            <CardContent className='p-4 gap-4'>
+              <div className='pb-3 text-xl'>
+                Subtotal ({cart.items.reduce((a, c) => a + c.qty, 0)}):
+                <span className='font-bold'>
+                  {formatCurrency(cart.itemsPrice)}
+                </span>
+              </div>
+              <Button
+                className='w-full'
+                disabled={isPending}
+                onClick={() =>
+                  startTransition(() => router.push('/shipping-address'))
+                }
+              >
+                {isPending ? (
+                  <Loader className='w-4 h-4 animate-spin' />
+                ) : (
+                  <ArrowRight className='w-4 h-4' />
+                )}
+                Procedd to Checkout
+              </Button>
+            </CardContent>
+          </Card>
         </div>
       )}
     </>
